ba4d036f2f0a5993a9e2717aa7a2b1fa5ff23d43
Nihal Jere <noocsharp@gmail.com>
2016-12-11 12:01:22 -0600

    Created NonPlayer and made Door a NonPlayer

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <curses.h>
+#include <list>
 
-void DrawDialog(WINDOW *win);
-void gameLoop();
-void IncrementDialogState(WINDOW * win);
-void DecrementDialogState(WINDOW * win);
-int DialogState = 0;
-int DialogLength = 4;
-int segment = 0;
+using namespace std;
 
-class Door {
+
+
+class NonPlayer {
+    
     public:
-        Door(int ix, int iy, int iw, int ih);
-        void Draw(WINDOW *win);
+        NonPlayer(int ix, int iy, int iw, int ih);
+        virtual void Draw(WINDOW *win);
         int GetX() const { return x; };
         void SetX(int ix) {x = ix;};
         int GetY() const { return y; };
@@ -20,9 +18,24 @@ class Door {
         void SetW(int iw) {w = iw;};
         int GetH() const { return h; };
         void SetH(int ih) {h = ih;};
-        
-    private:
+
+    protected:
         int x, y, w, h;
+};
+
+void DrawDialog(WINDOW *win);
+void gameLoop();
+void IncrementDialogState(WINDOW * win);
+void DecrementDialogState(WINDOW * win);
+void DrawLevel(WINDOW * win, list<NonPlayer*>);
+int DialogState = 0;
+int DialogLength = 4;
+int segment = 0;
+
+class Door : public NonPlayer {
+    public:
+        Door(int ix, int iy, int iw, int ih):NonPlayer(ix, iy, iw, ih){};
+        virtual void Draw(WINDOW *win);
 };
 
 class Player {
@@ -41,6 +54,14 @@ class Player {
         int x, y, w, h;
 };
 
+NonPlayer::NonPlayer(int ix, int iy, int iw, int ih) {
+    x = ix;
+    y = ix;
+    w = iw;
+    h = ih;
+}
+
+
 Player::Player(int ix, int iy) {
     x = ix;
     y = ix;
@@ -48,33 +69,31 @@ Player::Player(int ix, int iy) {
     h = 3;
 }
 
-Door::Door(int ix, int iy, int iw, int ih) {
-    x = ix;
-    y = ix;
-    w = iw;
-    h = ih;
+
+void NonPlayer::Draw(WINDOW *win) {
+    mvwaddch(win, x, y, 'N');
 }
 
 void Player::Draw(WINDOW *win) {
     
-    mvwaddch(win, y, x, '@');
-    mvwaddch(win, y+1, x, '|');
-    mvwaddch(win, y+1, x-1, '-');
-    mvwaddch(win, y+1, x+1, '-');
-    mvwaddch(win, y+2, x+1, '\\');
-    mvwaddch(win, y+2, x-1, '/');
+    mvwaddch(win, y, x+1, '@');
+    mvwaddch(win, y+1, x+1, '|');
+    mvwaddch(win, y+1, x, '-');
+    mvwaddch(win, y+1, x+2, '-');
+    mvwaddch(win, y+2, x+2, '\\');
+    mvwaddch(win, y+2, x, '/');
 }
 
 void Door::Draw(WINDOW *win) {
     
 
-    for (int j = 0; j<h; j++) {
+    for (int j = 0; j<h; ++j) {
     
         mvwaddch(win, y+j, x, '|');
         mvwaddch(win, y+j, x+w-1, '|');
     }
 
-    for (int i = 0; i<w; i++) {
+    for (int i = 0; i<w; ++i) {
         
         mvwaddch(win, y, x+i, '-');
         mvwaddch(win, y+h-1, x+i, '-');
@@ -82,14 +101,15 @@ void Door::Draw(WINDOW *win) {
 }
 
 bool Player::HasCollidedWithDoor(Door * door) {
-    if ((x+w>=door->GetX() && x+w<=door->GetX()+door->GetW() && y+h>=door->GetY() && y+h<=door->GetY()+door->GetH()) ||
-        (x>=door->GetX() && x<=door->GetX()+door->GetW() && y+h>=door->GetY() && y+h<=door->GetY()+door->GetH()) ||
-        (x>=door->GetX() && x<=door->GetX()+door->GetW() && y>=door->GetY() && y<=door->GetY()+door->GetH()) ||
-        (x+w>=door->GetX() && x+w<=door->GetX()+door->GetW() && y>=door->GetY() && y<=door->GetY()+door->GetH())) {
+    if ((x+w-1>=door->GetX() && x+w-1<=door->GetX()+door->GetW()-1 && y+h-1>=door->GetY() && y+h-1<=door->GetY()+door->GetH()-1) ||
+        (x>=door->GetX() && x<=door->GetX()+door->GetW()-1 && y+h-1>=door->GetY() && y+h-1<=door->GetY()+door->GetH()-1) ||
+        (x>=door->GetX() && x<=door->GetX()+door->GetW()-1 && y>=door->GetY() && y<=door->GetY()+door->GetH()-1) ||
+        (x+w-1>=door->GetX() && x+w-1<=door->GetX()+door->GetW()-1 && y>=door->GetY() && y<=door->GetY()+door->GetH()-1)) {
         return true;
     }
     return false;
 }
+
 int main()
 {
     initscr();
@@ -102,8 +122,10 @@ int main()
 void gameLoop()
 {
     int ch;
-    Player player = Player(10, 10);
-    Door door = Door(30, 10, 5, 6);
+    Player player(10, 10);
+    list<NonPlayer*> NonPlayerObjects;
+    NonPlayerObjects.push_back(new Door(30, 10, 6, 6));
+    list<NonPlayer*>::iterator NPOIterator = NonPlayerObjects.begin();
     int x = 5;
     int y = 5;
     const int DIALOG_HEIGHT = 3;
@@ -114,7 +136,7 @@ void gameLoop()
     werase(GameArea);
     while (true) {
         DrawDialog(Dialog);
-        for (int i = 0; i<WIDTH; i++) {
+        for (int i = 0; i<WIDTH; ++i) {
             mvwaddch(Dialog, DIALOG_HEIGHT-1, i, '-');
             wnoutrefresh(Dialog);
             doupdate();
@@ -123,6 +145,9 @@ void gameLoop()
             continue;
         } else {
             if (ch == 'q') {
+                for (list<NonPlayer*>::iterator n = NonPlayerObjects.begin(); n != NonPlayerObjects.end(); ++n) {
+                    delete *n;
+                }
                 endwin();
                 break;
             }
@@ -130,7 +155,7 @@ void gameLoop()
                 case 'w':
                     werase(GameArea);
                     player.SetY(player.GetY()-1);
-                    if (player.HasCollidedWithDoor(&door)) {
+                    if (player.HasCollidedWithDoor(static_cast<Door*>(*NPOIterator))) {
                         IncrementDialogState(Dialog);
                     }
                     player.Draw(GameArea);
@@ -139,18 +164,27 @@ void gameLoop()
                 case 's':
                     werase(GameArea);
                     player.SetY(player.GetY()+1);
+                    if (player.HasCollidedWithDoor(static_cast<Door*>(*NPOIterator))) {
+                        IncrementDialogState(Dialog);
+                    }
                     player.Draw(GameArea);
                     break;
 
                 case 'a':
                     werase(GameArea);
                     player.SetX(player.GetX()-1);
+                    if (player.HasCollidedWithDoor(static_cast<Door*>(*NPOIterator))) {
+                        IncrementDialogState(Dialog);
+                    }
                     player.Draw(GameArea);
                     break;
 
                 case 'd':
                     werase(GameArea);
                     player.SetX(player.GetX()+1);
+                    if (player.HasCollidedWithDoor(static_cast<Door*>(*NPOIterator))) {
+                        IncrementDialogState(Dialog);
+                    }
                     player.Draw(GameArea);
                     break;
                 
@@ -166,7 +200,7 @@ void gameLoop()
 
             }
 
-        door.Draw(GameArea);
+        (*NPOIterator)->Draw(GameArea);
         wnoutrefresh(GameArea);
         doupdate();
         }
@@ -207,5 +241,15 @@ void DecrementDialogState(WINDOW * win) {
     if (DialogState >=1 && DialogState<=DialogLength-1) {
         DialogState -= 1;
         DrawDialog(win);
+    }
+}
+
+
+void DrawLevel(WINDOW * win, list<NonPlayer*> objs) {
+    switch (segment) {
+
+    }
+    for (list<NonPlayer*>::iterator n = objs.begin(); n != objs.end(); ++n) {
+        (*n)->Draw(win);
     }
 }
